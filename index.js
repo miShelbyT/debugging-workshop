ebc283dac149f3ee8cd9a5a651a2c7e175a585df
miShelbyT <mi.shelbyrose@gmail.com>
2020-11-23 10:47:15 -0500

    done yay

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
-
+  
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+      // console.log(jokeData)
+      joke = jokeData.joke
+    })
+      
   }
-
+  
   form.addEventListener('submit', (event) => {
-
+    event.preventDefault()
+    const username = document.getElementById('name-input').value
+    const newJokeLi = document.createElement('li')
     if(username === "") return;
     fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
+    .then(() => {
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+    })
+    
     jokeList.appendChild(newJokeLi)
+    event.target.reset()
   })
 })
